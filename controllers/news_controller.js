546a644b994e45f50740b7d8f25c1a12c29a5f11
Laura McGinn <mcginn.laura@gmail.com>
2017-06-03 21:21:42 -0500

    basic scrape running

--- controllers/news_controller.js
+++ controllers/news_controller.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var path = require('path');
 var cheerio = require('cheerio');
+var request = require('request');
 
 module.exports = function(app) {
 
@@ -11,8 +12,35 @@ module.exports = function(app) {
 		res.render('index');
 	});
 
-	// app.get('/scrape', function(req, res) )
+	app.get('/scrape', function(req, res) {
+		request('http://www.reddit.com/r/futurology', function (error, response, html) {
+			var $cheerio = cheerio.load(html);
 
-	
+			var result = [];
+
+			$cheerio('a.title').each(function(error, response) {
+
+				if (error) {
+					console.log('There was an error: ' + error);
+				}				
+
+				//pulling the title and the source link
+				var title = $cheerio(this).text();
+				var sourceLink = $cheerio(this).children().attr('href');
+
+				result.push({
+					title: title,
+					sourceLink: sourceLink
+				});
+			});
+
+			console.log(result);
+			
+		});
+
+
+	});
+
+
 //end module.exports
 }
