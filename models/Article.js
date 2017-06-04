995617d26909a5a2af49a913fbe73f6c5c247eec
Laura McGinn <mcginn.laura@gmail.com>
2017-06-03 22:50:48 -0500

    revisions to model files

--- models/Article.js
+++ models/Article.js
@@ -4,14 +4,17 @@ var mongoose = require('mongoose');
 //creating schema class
 var Schema = mongoose.Schema;
 
+//creating article schema
 var ArticleSchema = new Schema({
 	title: {
 		type: String,
 		required: true
 	},
+	//sourceLink is unique to prevent storing duplicate articles
 	sourceLink: {
 		type: String,
 		required: true,
+		unique: true
 	},
 	comment: {
 		type: Schema.Types.ObjectId,
